f80de47b2d367485925d5f6728e353b956243eb9
Franjo Dumanovsky <franjodumanovsky@gmail.com>
2023-07-31 19:11:31 +0200

    progress from day02 to day04

--- day-01/phpmailer/index.php
+++ day-01/phpmailer/index.php
@@ -24,11 +24,7 @@ $errorMessage = '';
 if (!empty($_POST)) {
    $name = $_POST['name'];
    $email = $_POST['email'];
-   $message = $_POST['message'];
-   $full_number = $_POST['full_number'];
-   $dateFor = $_POST['date-for'];
-   $numberOfPeople = $_POST['numberOfPeople'];
-   $foundAboutUs = $_POST['foundAboutUs'];
+   $phone = $_POST['phone'];
 
 // specify SMTP credentials
 	$mail = new PHPMailer();
@@ -43,33 +39,40 @@ if (!empty($_POST)) {
 //Port to connect smtp
 	$mail->Port = "587";
 //Set gmail username
-	$mail->Username = "barcrawlzagreb@gmail.com";
+	$mail->Username = "dumanovskyfinance@gmail.com";
 //Set gmail password
-	// $mail->Password = "yykmxheirqsjfsbx";
-	$mail->Password = "bwcdoiiyzvsnbnne";
+	$mail->Password = "yykmxheirqsjfsbx";
+
 //Email subject
-	$mail->Subject = "Bar crawl Zagreb - Form submission";
+	$mail->Subject = "100DaysOfCode";
 //Set sender email
 	$mail->setFrom($email);
 //Enable HTML
 	$mail->isHTML(true);
 //Attachment
 	// $mail->addAttachment('img/attachment.png');
-//Email body
-	// $mail->Body = "<h1>This is HTML h1 Heading</h1></br><p>This is html paragraph</p>";
 //Add recipient
-	$mail->addAddress('barcrawlzagreb@gmail.com');
-       // Enable HTML if needed
-       $mail->isHTML(true);
-       $bodyParagraphs = ["<strong>Name:</strong> {$name}", "<strong>Email:</strong> {$email}", "<strong>Message:</strong>", nl2br($message),"<strong>Phone number:</strong> +{$full_number}", "<strong>Date:</strong> {$dateFor}", "<strong>Number of people:</strong> {$numberOfPeople}", "<strong>We found about you through:</strong> {$foundAboutUs}"];
-       $body = join('<br />', $bodyParagraphs);
-       $mail->Body = $body;
-       echo $body;
+	$mail->addAddress('dumanovskyfinance@gmail.com');
+// Enable HTML if needed
+    $mail->isHTML(true);
+
+// Array of elements to be sent..
+       
+	$bodyParagraphs = ["<strong>Name:</strong> {$name}", "<strong>Email:</strong> {$email}", "<strong>Phone:</strong> {$phone}"];
+
+
+    $body = join('<br />', $bodyParagraphs);
+
+    $mail->Body = $body;
+
+    echo $body;
 
 
 	if ( $mail->send() ) {
+
 		echo "Email Sent..!";
-	}else{
+	}else {
+
 		echo "Message could not be sent. Mailer Error: ";
 	}
    }
---
+++ day-01/src/script.js
@@ -0,0 +1,18 @@
+const inputFields = document.querySelectorAll("form input");
+const header = document.querySelector(".header");
+const para = document.querySelector(".para");
+
+window.addEventListener("click", function () {
+  console.log(inputFields[0] === document.activeElement);
+
+  for (let i = 0; i < inputFields.length; i++) {
+    if (inputFields[i] === document.activeElement) {
+      header.classList.add("active-anim");
+      para.classList.add("active");
+      break;
+    } else {
+      header.classList.remove("active-anim");
+      para.classList.remove("active");
+    }
+  }
+});
